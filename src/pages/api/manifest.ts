e94c71c2a4dfdc2659fd7cd342ae4ca7b46f6b8a
Tien Do <tiendq@gmail.com>
2023-03-16 17:17:07 +0700

    refactor

--- src/pages/api/manifest.ts
+++ src/pages/api/manifest.ts
@@ -1,45 +1,25 @@
-import { createManifestHandler } from "@saleor/app-sdk/handlers/next";
 import { AppManifest } from "@saleor/app-sdk/types";
-
+import { createManifestHandler } from "@saleor/app-sdk/handlers/next";
 import packageJson from "../../../package.json";
 
 export default createManifestHandler({
   async manifestFactory(context) {
     const manifest: AppManifest = {
-      name: packageJson.name,
-      tokenTargetUrl: `${context.appBaseUrl}/api/register`,
-      appUrl: context.appBaseUrl,
-      permissions: [
-        /**
-         * Set permissions for app if needed
-         * https://docs.saleor.io/docs/3.x/developer/permissions
-         */
-        "MANAGE_CHECKOUTS"
-      ],
-      id: "saleor.app",
+      name: "Abandoned Checkouts",
+      id: "abandoned.checkouts.app",
       version: packageJson.version,
-      webhooks: [
-        /**
-         * Configure webhooks here. They will be created in Saleor during installation
-         * Read more
-         * https://docs.saleor.io/docs/3.x/developer/api-reference/objects/webhook
-         */
-      ],
-      extensions: [
-        /**
-         * Optionally, extend Dashboard with custom UIs
-         * https://docs.saleor.io/docs/3.x/developer/extending/apps/extending-dashboard-with-apps
-         */
-        {
-          label: "Abandoned Checkouts",
-          mount: "NAVIGATION_ORDERS",
-          target: "APP_PAGE",
-          permissions: ["MANAGE_CHECKOUTS"],
-          url: "/",
-        }
-      ],
+      appUrl: context.appBaseUrl,
+      tokenTargetUrl: `${context.appBaseUrl}/api/register`,
+      permissions: ["MANAGE_CHECKOUTS"],
+      extensions: [{
+        label: "Abandoned Checkouts",
+        mount: "NAVIGATION_ORDERS",
+        target: "APP_PAGE",
+        permissions: ["MANAGE_CHECKOUTS"],
+        url: "/"
+      }]
     };
 
     return manifest;
-  },
+  }
 });
